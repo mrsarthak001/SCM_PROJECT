304877d5302270f2cf04a8bde363faf96e983166
Kshitiz <kshitizsaini.rtk@gmail.com>
2019-04-16 22:51:48 +0530

    Basic structure created

---
+++ code.cpp
@@ -0,0 +1,18 @@
+// Declaration of libraries
+
+#include <iostream.h>
+#include <conio.h>
+#include <iomanip.h>
+#include <stdlib.h>
+#include <graphics.h>
+#include <stdio.h>
+#include <dos.h>
+
+//MAIN PROGRAM
+
+void main()
+{
+
+
+
+}
